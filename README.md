e3013f749377887d129c4669ba145bc78694ba51
Holger Brandl <holgerbrandl@gmail.com>
2016-06-08 00:46:55 +0200

    started project

---
+++ README.md
@@ -0,0 +1,10 @@
+Paste Image To Markdown
+=======================
+
+
+ Paste images directly from clipboard into Markdown Documents in Intellij. Images files are deposited in a configurable directory relative to the markdown.
+
+
+## Installation
+
+Use the plugin browser in Intellij to download the plugin.
---
+++ src/img2md/PasteImageFromClipboard.java
@@ -0,0 +1,15 @@
+package img2md;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * Created by brandl on 6/8/16.
+ */
+public class PasteImageFromClipboard extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
